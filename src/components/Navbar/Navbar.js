ef1959b9eaf820e8931a3cbf2ce52c939e77c066
Hon <dennis.mejen@gmail.com>
2020-01-14 10:21:15 +0700

    Add Navbar-Icons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './Navbar.css'
-import {Row, Col} from 'antd'
+import {Row, Col, Icon} from 'antd'
 
 export default class Navbar extends Component {
   render() {
@@ -8,9 +8,16 @@ export default class Navbar extends Component {
       <div>
         <Row className="Navbar-Main">
           <Col span={20}>
-            <img src="maidProServiceLogo.png" alt="" width="125" />
+            <a href="http://localhost:3000/">
+              <img src="maidProServiceLogo.png" alt="" width="125" />
+            </a>
           </Col>
-          <Col span={4}></Col>
+          <Col span={4}>
+            <Row type="flex">
+              <Icon type="search" className="Navbar-Icon" />
+              <Icon type="menu" className="Navbar-Icon" />
+            </Row>
+          </Col>
         </Row>
         <Row className ="Navbar-Border"></Row>
       </div>
