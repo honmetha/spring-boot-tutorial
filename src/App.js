f2d4e4a60e0ffb300c1342ec3b9f75c3a7af823e
Hon <dennis.mejen@gmail.com>
2020-01-18 00:18:38 +0700

    Add LoginPage

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { Route, Switch } from 'react-router-dom'
 import { Redirect } from 'react-router'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
-import Login from './pages/Login'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/login" component={LoginPage} />
         <Route exact path="/signup" component={Signup} />
         <Redirect to="/" />
       </Switch>
