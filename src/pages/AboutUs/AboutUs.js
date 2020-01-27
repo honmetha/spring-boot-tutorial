85e61ac7b09af246b83869b8dab75fa8d2005a57
Hon <dennis.mejen@gmail.com>
2020-01-27 12:17:35 +0700

    Save Progress on AboutUs page

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -9,14 +9,26 @@ export default class AboutUs extends Component {
     return (
       <div>
         <Navbar />
-        <Row type="flex" justify="center">
-          <Col>
-            <h1>How it All Began</h1>
-            <p>
-              Merry Maids® was founded in 1979 in Omaha, Nebraska by Dallen Peterson. Dallen worked with his family to create the Merry Maids® system and franchise the company. In 1988, ServiceMaster purchased Merry Maids® from the Petersons. All ServiceMaster companies specialize in home services, so Merry Maids® has been a natural fit for ServiceMaster from the beginning. Some of our sister companies in our Family of Brands include Amerispec, Furniture Medic, Terminix, ServiceMaster Restore, and ServiceMaster Clean.
-
-              Merry Maids® currently has approximately 485 locations throughout the United States. As we have grown, our core values remain the heart of what we do and who we are. Many of the franchise owners who purchased territories in the early years have included their children in their businesses and continue to work themselves. Merry Maids® also encourages career growth. In fact, several branch managers began their careers as team members.
-            </p>
+        <Row type="flex" justify="center" className="AboutUs-Content">
+          <Col xs={22} sm={24} md={20} lg={18}>
+            <Row type="flex" justify="center">
+              <h1>- Maid Pro Service -</h1>
+            </Row>
+            <Row type="flex" justify="center">
+              <p>To provide the most satisfied service with efficiency.</p>
+            </Row>
+            <Row type="flex" justify="center">
+              <h1>How it All Began</h1>
+            </Row>
+            <Row>
+              <p>
+                Maid Pro Service® was founded in 2020 in Bangkok, Thailand by the team of young entrepreneurs. The team worked with Code Camp 4 to create Maid Pro Service® system and franchise the company.
+                After 2 months, Airbnb purchased Maid Pro Service® from the young entrepreneurs. Airbnb had properties around the world, so Maid Pro Service® has been a natural fit for Airbnb from the beginning.
+                Some of our sister companies in our Family of Brands include Maidfun, Maidlover and ServiceMaster Clean. Maid Pro Service® currently has approximately 485 locations throughout Thailand.
+                As we have grown, our core values remain the heart of what we do and who we are. Many of our founders who started this business are still working themselves until today.
+                Maid Pro Service® also encourages career growth. In fact, several branch managers began their careers as team members.
+              </p>
+            </Row>
           </Col>
         </Row>
         <Footer />
