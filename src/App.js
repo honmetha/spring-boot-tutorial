3d85a3a82fc7a791de8448e0a527dcc5119eaf56
Hon <dennis.mejen@gmail.com>
2020-03-05 11:51:27 +0700

    Delete previous group project

--- src/App.js
+++ src/App.js
@@ -4,28 +4,12 @@ import 'antd/dist/antd.css';
 import { Route, Switch } from 'react-router-dom'
 import { Redirect } from 'react-router'
 import Home from './pages/Home/Home'
-import LoginPage from './pages/LoginPage/LoginPage'
-import Registration from './pages/Registration/Registration'
-import RegistrationMaid from './pages/RegistrationMaid/RegistrationMaid'
-import MaidDescription from './pages/MaidDescription/MaidDescription'
-import MyBookingHistory from './pages/MyBookingHistory/MyBookingHistory'
-import AboutUs from './pages/AboutUs/AboutUs'
-import Payment from './pages/Payment/Payment'
-import Policies from './pages/Policies/Policies'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/register" component={Registration} />
-        <Route exact path="/registermaid" component={RegistrationMaid} />
-        <Route exact path="/maiddescription" component={MaidDescription} />
-        <Route exact path="/bookinghistory" component={MyBookingHistory} />
-        <Route exact path="/aboutus" component={AboutUs} />
-        <Route exact path="/payment" component={Payment} />
-        <Route exact path="/policies" component={Policies} />
         <Redirect to="/" />
       </Switch>
     </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,58 +1,10 @@
 import React, { Component } from 'react'
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Footer/Footer'
-import { Row, Col, Carousel, Button } from 'antd';
-import { FaBuilding, FaHome } from "react-icons/fa";
-import Carousel1 from '../../images/Carousel1.jpeg'
-import Carousel2 from '../../images/Carousel2.jpg'
-import Carousel3 from '../../images/Carousel3.jpg'
 
-export default class ChangePassword extends Component {
+export default class Home extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <Carousel autoplay>
-          <div>
-            <img src={Carousel1} alt="" width="100%" />
-          </div>
-          <div>
-            <img src={Carousel2} alt="" width="100%" />
-          </div>
-          <div>
-            <img src={Carousel3} alt="" width="100%" />
-          </div>
-        </Carousel>
-        <Row type="flex" justify="center">
-              <h2>Services</h2>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <Row type="flex" justify="end">
-                  <Button className="HomePage-ServiceButtons">
-                    <Col className="HomePage-ButtonIconColumn">
-                      <FaBuilding className="HomePage-ButtonColumnIcon" />
-                    </Col>
-                    <Col>Condo</Col>
-                  </Button>
-                </Row>
-              </Col>
-              <Col span={12}>
-                <Row type="flex" justify="start">
-                  <Button className="HomePage-ServiceButtons">
-                    <Col className="HomePage-ButtonIconColumn">
-                      <FaHome className="HomePage-ButtonColumnIcon" />
-                    </Col>
-                    <Col>Home</Col>
-                  </Button>
-                </Row>
-              </Col>
-            </Row>
-            <Row type="flex" justify="center">
-              <h2> Maid Recommended For You </h2>
-            </Row>
-        <Footer />
+        Hello World
       </div>
     )
   }
