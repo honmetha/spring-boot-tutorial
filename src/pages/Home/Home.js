ff95103ec66aa1ae56c1f7675ad8930dc164244e
Hon <dennis.mejen@gmail.com>
2020-03-05 12:16:55 +0700

    Add layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Layout, Menu } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Layout>
+          <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
+            onBreakpoint={broken => {
+              console.log(broken);
+            }}
+            onCollapse={(collapsed, type) => {
+              console.log(collapsed, type);
+            }}
+          >
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+              <Menu.Item key="1">
+                <span className="nav-text">เครื่องทั้งหมด</span>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <span className="nav-text">เพิ่มเครื่องของลูกค้า</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header
+              className="site-layout-sub-header-background"
+              style={{ padding: 0 }}
+            />
+            <Content style={{ margin: "24px 16px 0" }}>
+              <div
+                className="site-layout-background"
+                style={{ padding: 24, minHeight: 360 }}
+              >
+                content
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+              Ant Design ©2018 Created by Ant UED
+            </Footer>
+          </Layout>
+        </Layout>
+        ,
       </div>
-    )
+    );
   }
 }
