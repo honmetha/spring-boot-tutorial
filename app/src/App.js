bc4fe8aa78259f7d3012a8dafaebe1bca3a843c1
Hon <dennis.mejen@gmail.com>
2020-06-05 23:59:37 +0700

    Add Introducing React Hooks 00:07:34 - 00:11:34

--- app/src/App.js
+++ app/src/App.js
@@ -5,6 +5,28 @@ const Todo = ({ todo, index }) => {
   return <div className="todo">{todo.text}</div>;
 };
 
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </form>
+  );
+};
+
 const App = () => {
   const [todos, setTodos] = useState([
     {
