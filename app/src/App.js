bbe11a07ae78323dddba165cf24466e76bc26696
Hon <dennis.mejen@gmail.com>
2020-06-06 14:34:49 +0700

    Add Introducing React Hooks 00:00:00 - 00:08:34

---
+++ app/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const Todo = ({ todo, index }) => {
+  return <div className="todo">{todo.text}</div>;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false,
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false,
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false,
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
