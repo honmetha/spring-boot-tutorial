12db79ce6a59153ae523e40978c9d1029eda9552
Hon <dennis.mejen@gmail.com>
2020-06-18 07:46:48 +0700

    Finish Introducing React Hooks 12

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
-
-const Todo = ({ todo, index }) => {
-  return <div className="todo">{todo.text}</div>;
-};
-
-const TodoForm = ({ addTodo }) => {
-  const [value, setValue] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
-    setValue("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        placeholder="Add Todo..."
-        onChange={(e) => setValue(e.target.value)}
-      />
-    </form>
-  );
-};
+import Todo from "./components/Todo";
+import TodoForm from "./components/TodoForm";
 
 const App = () => {
   const [todos, setTodos] = useState([
@@ -49,11 +24,29 @@ const App = () => {
     setTodos(newTodos);
   };
 
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
