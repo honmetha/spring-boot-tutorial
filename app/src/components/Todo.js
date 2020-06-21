c109ae6ac89d1e829f48eba37921753382e1e96b
Hon <dennis.mejen@gmail.com>
2020-06-21 23:16:36 +0700

    Add components and update README.md

---
+++ app/src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Todo = ({ todo, index, completeTodo, removeTodo }) => {
+  return (
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className="todo"
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>completeTodo</button>
+        <button onClick={() => removeTodo(index)}>x</button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo
