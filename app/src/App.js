37a88dfb1b9b1480b783f47dc4c4d8031e101c53
Hon <dennis.mejen@gmail.com>
2020-06-22 23:03:12 +0700

    Add todolist page

--- app/src/App.js
+++ app/src/App.js
@@ -1,57 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Todo from "./components/Todo";
-import TodoForm from "./components/TodoForm";
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      text: "Learn about React",
-      isCompleted: false,
-    },
-    {
-      text: "Meet friend for lunch",
-      isCompleted: false,
-    },
-    {
-      text: "Build really cool todo app",
-      isCompleted: false,
-    },
-  ]);
-
-  const addTodo = (text) => {
-    const newTodos = [...todos, { text }];
-    setTodos(newTodos);
-  };
-
-  const completeTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos[index].isCompleted = !newTodos[index].isCompleted;
-    setTodos(newTodos);
-  };
-
-  const removeTodo = (index) => {
-    const newTodos = [...todos];
-    newTodos.splice(index, 1);
-    setTodos(newTodos);
-  };
-
-  return (
-    <div className="app">
-      <div className="todo-list">
-        {todos.map((todo, index) => (
-          <Todo
-            key={index}
-            index={index}
-            todo={todo}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}
-          />
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
-    </div>
-  );
+  return <div>Hello World</div>;
 };
 
 export default App;
