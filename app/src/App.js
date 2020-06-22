e9d36061181c6701261ae569ed1ae9fc91a6aa8b
Hon <dennis.mejen@gmail.com>
2020-06-22 23:08:23 +0700

    Add home page and page switchable app

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/todolist/List";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todolist">Todo List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/todolist">
+            <List />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ app/src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      My Home Page
+    </div>
+  )
+}
+
+export default Home
