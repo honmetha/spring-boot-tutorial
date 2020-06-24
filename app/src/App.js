94697c05d9fc2c282ea68a7efb1e65a6c2ddaac3
Hon <dennis.mejen@gmail.com>
2020-06-24 07:49:54 +0700

    Add Build an Expense Tracker 00:00:00 - 00:07:00

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import List from "./pages/todoList/List";
 import Home from "./pages/home/Home";
+import TodoList from "./pages/todoList/List";
+import ExpenseTrackerList from './pages/expenseTracker/List'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/todolist">
-          <List />
+        <Route exact path="/todolist">
+          <TodoList />
+        </Route>
+        <Route exact path="/expensetracker">
+          <ExpenseTrackerList />
         </Route>
         <Route path="/">
           <Home />
---
+++ app/src/pages/expenseTracker/List.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./components/Header";
+import "./expenseTracker.css";
+
+const List = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default List;
