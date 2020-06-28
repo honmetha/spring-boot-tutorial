32cdcfcc1ddb9b316e2a2dd11eb09848e4b4ebd8
Hon <dennis.mejen@gmail.com>
2020-06-28 23:02:25 +0700

    [Added] Add Expense Tracker setup

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import TodoList from "./pages/todoList/List";
+import ExpenseTracker from "./pages/expenseTracker/List";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Switch>
         <Route exact path="/todolist">
           <TodoList />
+        </Route>
+        <Route exact path="/expensetracker">
+          <ExpenseTracker />
         </Route>
         <Route path="/">
           <Home />
