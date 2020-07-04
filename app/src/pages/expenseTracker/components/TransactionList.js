0b987d5b1933fac020208599eaa72c053eb167f7
Hon <dennis.mejen@gmail.com>
2020-07-04 12:25:14 +0700

    [Added] Add Build an Expense Tracker 00:15:45 - 00:31:40

--- app/src/pages/expenseTracker/components/TransactionList.js
+++ app/src/pages/expenseTracker/components/TransactionList.js
@@ -1,14 +1,17 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
+import { Transaction } from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <Fragment>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </Fragment>
   );
