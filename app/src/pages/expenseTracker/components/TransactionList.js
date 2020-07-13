2d7eba5265bcbd2ea99f39865d89ddb9783c3c78
Hon <dennis.mejen@gmail.com>
2020-07-13 10:46:38 +0700

    [Added] Add Build an Expense Tracker 00:00:00 - 00:12:20

---
+++ app/src/pages/expenseTracker/components/TransactionList.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+export const TransactionList = () => {
+  return (
+    <Fragment>
+      <h3>History</h3>
+      <ul id="list" class="list">
+        <li class="minus">
+          Cash <span>-$400</span><button class="delete-btn">x</button>
+        </li>
+      </ul>
+    </Fragment>
+  )
+}
