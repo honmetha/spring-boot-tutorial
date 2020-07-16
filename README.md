de5b122b4d8c09ba6d0dc9a5034bb64f2262d647
Hon <dennis.mejen@gmail.com>
2020-07-16 21:40:56 +0700

    [Added] Finish Build an Expense Tracker 6 and update README.md

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Introducing React Hooks = 16 <br />
 https://www.youtube.com/watch?v=mxK8b99iJTg
 
-Build an Expense Tracker | React Hooks & Context API = 5 <br />
+Build an Expense Tracker | React Hooks & Context API = 6 <br />
 https://www.youtube.com/watch?v=XuFDcZABiDQ&t=2151s
--- app/src/pages/expenseTracker/components/AddTransaction.js
+++ app/src/pages/expenseTracker/components/AddTransaction.js
@@ -1,13 +1,30 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState("");
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount,
+    };
+
+    addTransaction(newTransaction);
+    setText("");
+    setAmount("");
+  };
+
   return (
     <Fragment>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
