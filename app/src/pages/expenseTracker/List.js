1c04affbac1eae406758d62b3cb6c33df3183f6c
Hon <dennis.mejen@gmail.com>
2020-07-18 08:57:58 +0700

    [Added] Add Build an Expense Tracker 00:15:45 - 00:23:50

--- app/src/pages/expenseTracker/List.js
+++ app/src/pages/expenseTracker/List.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Header } from "./components/Header";
-import "./expenseTracker.css";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
+import "./expenseTracker.css";
 
 const List = () => {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -16,7 +17,7 @@ const List = () => {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 };
 
