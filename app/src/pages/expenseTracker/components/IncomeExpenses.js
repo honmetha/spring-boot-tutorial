c25aed7f3251c6ffd3379b6642e507966b073ea9
Hon <dennis.mejen@gmail.com>
2020-07-21 07:46:00 +0700

    [Added] Add Build an Expense Tracker 00:00:00 - 00:12:20

---
+++ app/src/pages/expenseTracker/components/IncomeExpenses.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+
+export const IncomeExpenses = () => {
+  return (
+    <Fragment>
+      <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+          <p className="money plus">+$0.00</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+          <p className="money minus">-$0.00</p>
+        </div>
+      </div>
+    </Fragment>
+  )
+}
