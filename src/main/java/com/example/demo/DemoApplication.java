48491ec6b91a730abec607473a04db3a61e83b3e
hon <dennis.mejen@gmail.com>
2022-07-10 00:16:24 +0700

    📦 add: student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +20,13 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(new Student(
+				1L,
+				"Mariam",
+				"mariam.jamal@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 5),
+				21
+		));
 	}
 }
