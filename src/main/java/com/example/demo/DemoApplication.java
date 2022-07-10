b56e7625e14802a95ae8606fda8c535c63ca5246
hon <dennis.mejen@gmail.com>
2022-07-10 23:51:59 +0700

    📦 add: api layer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,32 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(new Student(
-				1L,
-				"Mariam",
-				"mariam.jamal@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-		));
-	}
 }
